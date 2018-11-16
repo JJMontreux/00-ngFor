e9a920f2bd814bdca137fbecf40dc0517b91acd6
S.Giorgetta <Shinji_Ikari_1983@hotmail.com>
2018-11-16 16:25:19 +0100

    Tarea ngFor funcionando

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  title = 'Tour of Heroes';
+
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+
+  myHero = this.heroes[0];
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
+
